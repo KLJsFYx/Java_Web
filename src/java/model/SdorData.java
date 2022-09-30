12d5be4b17e6343e4b75ec282d8e9f2f09fa60ce
KNPP <114608495+KLJsFYx@users.noreply.github.com>
2022-09-30 17:53:56 +0700

    Add files via upload

---
+++ src/java/model/SdorData.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author tokyo
+ */
+public class SdorData {
+
+    private String NameS;
+    private String Sid;
+    private String Sex;
+    private String Classyear;
+    private int Phone;
+    private int PPhone;
+    private String Pname;
+
+    public String getNameS() {
+        return NameS;
+    }
+
+    public void setNameS(String NameS) {
+        this.NameS = NameS;
+    }
+
+    public String getSid() {
+        return Sid;
+    }
+
+    public void setSid(String Sid) {
+        this.Sid = Sid;
+    }
+
+    public String getSex() {
+        return Sex;
+    }
+
+    public void setSex(String Sex) {
+        this.Sex = Sex;
+    }
+
+    public String getClassyear() {
+        return Classyear;
+    }
+
+    public void setClassyear(String Classyear) {
+        this.Classyear = Classyear;
+    }
+
+    public int getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(int Phone) {
+        this.Phone = Phone;
+    }
+
+    public int getPPhone() {
+        return PPhone;
+    }
+
+    public void setPPhone(int PPhone) {
+        this.PPhone = PPhone;
+    }
+
+    public String getPname() {
+        return Pname;
+    }
+
+    public void setPname(String Pname) {
+        this.Pname = Pname;
+    }
+}
+
+   
